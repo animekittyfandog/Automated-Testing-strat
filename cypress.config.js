5ffd38cd528b7277572b2bc2d1203c5bcb6a6ba1
animekittyfandog <elijahwaltererice@gmail.com>
2025-11-30 01:29:09 +0800

    support added

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8000',
+    supportFile: false,  // disables support file
     specPattern: 'cypress/integration/**/*.js',
-    supportFile: 'cypress/support/index.js',
-  },
+  }
 })
---
+++ cypress/support/index.js
@@ -0,0 +1,4 @@
+// cypress/support/index.js
+// This file is processed and loaded automatically before your test files.
+// You can put global configuration and behavior here.
+
