d6a48b92ef1f9b57db14797cb94239c8119b9b0c
animekittyfandog <elijahwaltererice@gmail.com>
2025-11-30 01:53:34 +0800

    file path fix 4

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8000',
+    baseUrl: 'http://localhost',
     supportFile: false,  // disables support file
     specPattern: 'cypress/integration/**/*.js',
   }
