be6f274127df55e7866cc414386b90a75a453aab
animekittyfandog <elijahwaltererice@gmail.com>
2025-11-30 02:05:51 +0800

    ci updated

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost',
+    baseUrl: 'http://localhost:8000',
     supportFile: false,  // disables support file
     specPattern: 'cypress/integration/**/*.js',
   }
