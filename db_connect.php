70320282454ed5c8dcc6f33437672b740dfa99d9
animekittyfandog <elijahwaltererice@gmail.com>
2025-11-30 12:46:36 +0800

    connect pass updated

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "127.0.0.1";
 $username = "root"; 
-$password = "";     
+$password = "root";     
 $dbname = "parksense_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname, 3306);
